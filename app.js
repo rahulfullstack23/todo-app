f3dcbbfe8a73a3f4918250fe92aec463428efe48
Rahul Kumar <hmko5432@gmail.com>
2026-06-23 12:05:11 +0530

    add enter and alert

--- app.js
+++ app.js
@@ -6,36 +6,41 @@ const list = document.getElementById("todo-list");
 
 addBtn.addEventListener("click", addTodo);
 
-function addTodo(){
+function addTodo() {
   const text = input.value.trim();
-  if(text===""){
-    // list.innerText='Please enter a task';
+  if (text === "") {
+    alert("Please enter a task");
     return;
   }
-  const li=document.createElement('li');
-  const checkbox=document.createElement('input');
-  checkbox.type="checkbox";
-  checkbox.addEventListener("change", function(){
-    if(checkbox.checked){
-      li.style.textDecoration="line-through";
-    }else{
-      li.style.textDecoration="none";
+  const li = document.createElement("li");
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.addEventListener("change", function () {
+    if (checkbox.checked) {
+      li.style.textDecoration = "line-through";
+    } else {
+      li.style.textDecoration = "none";
     }
   });
 
-const delBtn=document.createElement("button");
-delBtn.textContent="Delete";
+  const delBtn = document.createElement("button");
+  delBtn.textContent = "Delete";
 
-delBtn.addEventListener("click", function(){
-  li.remove();
-});
+  delBtn.addEventListener("click", function () {
+    li.remove();
+  });
 
-li.appendChild(checkbox);
+  li.appendChild(checkbox);
 
-li.append(text);
-li.appendChild(delBtn);
-list.appendChild(li);
+  li.append(" " + text + " ");
+  li.appendChild(delBtn);
+  list.appendChild(li);
 
-input.value="";
-
+  input.value = "";
 }
+
+input.addEventListener("keydown", function (e) {
+  if (e.key === "Enter") {
+    addTodo();
+  }
+});
